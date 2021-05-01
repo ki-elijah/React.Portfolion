a954deb764409440110babee8b4e7ecafc297c99
elijah <elijahknsubuga@gmail.com>
2021-05-01 17:27:37 +0300

    fixed bug

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectId: "7ntr6pm3",
-    dataset: "production"
-})
+  projectId: "le9a1mq2",
+  dataset: "production",
+});
