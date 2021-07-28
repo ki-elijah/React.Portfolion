176430835939a28e85b7e3be74d476a3c4bf1abd
elijah <elijahknsubuga@gmail.com>
2021-07-28 09:47:32 +0300

    added the lintree link

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
